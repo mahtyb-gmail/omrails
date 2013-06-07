7d846284e40fd436589f0ede20180b15d74d5b03
Martin Burke <mahtyb@gmail.com>
2013-06-06 18:00:54 -0700

    Remove skills from db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-skills = Skill.create([{name: 'Writers Unit'}, {name: 'Directors Unit'}, {name: 'Producers Unit'} ])
